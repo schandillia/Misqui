2b6b1e9ed85b20824c051c7f5ef9c2d5829bab7e
Amit Schandillia <amit@schandillia.com>
2025-04-23 15:39:03 +0530

    feat(courses): add getCourses query and render data in courses/page.tsx

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -1,7 +1,12 @@
-const Page = () => {
+import { getCourses } from "@/db/queries"
+
+const Page = async () => {
+  const data = await getCourses()
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="font-bold text-2xl text-neutral-700">Courses</h1>
+
+      {JSON.stringify(data)}
     </div>
   )
 }
