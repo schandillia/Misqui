27297df84f9167b23724b009dfe50834c2abfc6e
Amit Schandillia <amit@schandillia.com>
2025-04-23 17:35:08 +0530

    style(courses): update course card border radius from rounded-md to rounded-2xl

--- src/app/(main)/courses/card.tsx
+++ src/app/(main)/courses/card.tsx
@@ -23,13 +23,13 @@ export const Card = ({
     <div
       onClick={() => onClick(id)}
       className={cn(
-        "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]",
+        "h-full border-2 rounded-2xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]",
         disabled && "pointer-events-none opacity-50"
       )}
     >
       <div className="w-full min-h-[24px] justify-end flex items-center">
         {active && (
-          <div className="rounded-md bg-brand-600 p-1.5 justify-center items-center flex">
+          <div className="rounded-full bg-brand-600 p-1.5 justify-center items-center flex">
             <Check className="text-white stroke-[4] size-4" />
           </div>
         )}
