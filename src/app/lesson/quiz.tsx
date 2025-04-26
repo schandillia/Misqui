e8fd49ec2702b7793cf4c229a43830ab2a247456
Amit Schandillia <amit@schandillia.com>
2025-04-26 12:54:23 +0530

    feat(quiz): add logic to verify if the submitted answer is correct

--- src/app/lesson/quiz.tsx
+++ src/app/lesson/quiz.tsx
@@ -49,10 +49,41 @@ export const Quiz = ({
   const challenge = challenges[activeIndex]
   const options = challenge?.challengeOptions ?? []
 
+  const onNext = () => {
+    setActiveIndex((current) => current + 1)
+  }
+
   const onSelect = (id: number) => {
     if (status !== "none") return
 
     setSelectedOption(id)
+  }
+
+  const onContinue = () => {
+    if (!selectedOption) return
+
+    if (status === "wrong") {
+      setStatus("none")
+      setSelectedOption(undefined)
+      return
+    }
+
+    if (status === "correct") {
+      onNext()
+      setStatus("none")
+      setSelectedOption(undefined)
+      return
+    }
+
+    const correctOption = options.find((option) => option.correct)
+
+    if (!correctOption) return
+
+    if (correctOption && correctOption.id === selectedOption) {
+      console.log("CORRECT!")
+    } else {
+      console.error("INCORRECT")
+    }
   }
 
   const title =
@@ -89,7 +120,7 @@ export const Quiz = ({
           </div>
         </div>
       </div>
-      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
+      <Footer disabled={!selectedOption} status={status} onCheck={onContinue} />
     </>
   )
 }
