1a725f52a73969a24d4b6f8e847ef4e98b14e96b
Amit Schandillia <amit@schandillia.com>
2025-05-24 16:02:57 +0530

    chore: reimplement prettier-plugin-classnames and remove prettier-plugin-organize-imports

--- src/app/(main)/learn/lesson-banner.tsx
+++ src/app/(main)/learn/lesson-banner.tsx
@@ -54,7 +54,12 @@ export const LessonBanner = ({
   }
 
   return (
-    <div className="bg-brand-500 dark:bg-brand-800 flex w-full items-center justify-between rounded-3xl p-5 shadow-[inset_4px_4px_8px_rgba(0,0,0,0.1),inset_-4px_-4px_8px_rgba(255,255,255,0.2)] dark:shadow-[inset_4px_4px_8px_rgba(0,0,0,0.25),inset_-4px_-4px_8px_rgba(255,255,255,0.1)]">
+    <div
+      className="bg-brand-500 dark:bg-brand-800 flex w-full items-center justify-between
+        rounded-3xl p-5
+        shadow-[inset_4px_4px_8px_rgba(0,0,0,0.1),inset_-4px_-4px_8px_rgba(255,255,255,0.2)]
+        dark:shadow-[inset_4px_4px_8px_rgba(0,0,0,0.25),inset_-4px_-4px_8px_rgba(255,255,255,0.1)]"
+    >
       <div className="space-y-2.5 text-white dark:text-neutral-300">
         <h3 className="text-2xl font-bold">{title}</h3>
         <p className="text-lg">{description}</p>
