81bd9e7a7fcc908cf81f108ae5eebe9ec8aa4e16
Amit Schandillia <amit@schandillia.com>
2025-05-24 11:10:08 +0530

    style: emove text-shadow from lesson banner

--- src/app/(main)/learn/lesson-banner.tsx
+++ src/app/(main)/learn/lesson-banner.tsx
@@ -56,12 +56,8 @@ export const LessonBanner = ({
   return (
     <div className="bg-brand-500 dark:bg-brand-800 flex w-full items-center justify-between rounded-3xl p-5 shadow-[inset_8px_8px_12px_rgba(0,0,0,0.15),inset_-8px_-8px_12px_rgba(255,255,255,0.3)] dark:shadow-[inset_8px_8px_12px_rgba(0,0,0,0.35),inset_-8px_-8px_12px_rgba(255,255,255,0.15)]">
       <div className="space-y-2.5 text-white dark:text-neutral-300">
-        <h3 className="text-2xl font-bold text-shadow-[1px_1px_2px_rgba(255,255,255,0.5),-1px_-1px_2px_rgba(0,0,0,0.3)] dark:text-shadow-[1px_1px_2px_rgba(255,255,255,0.3),-1px_-1px_2px_rgba(0,0,0,0.5)]">
-          {title}
-        </h3>
-        <p className="text-lg text-shadow-[1px_1px_2px_rgba(255,255,255,0.5),-1px_-1px_2px_rgba(0,0,0,0.3)] dark:text-shadow-[1px_1px_2px_rgba(255,255,255,0.3),-1px_-1px_2px_rgba(0,0,0,0.5)]">
-          {description}
-        </p>
+        <h3 className="text-2xl font-bold">{title}</h3>
+        <p className="text-lg">{description}</p>
       </div>
       <Button size="lg" variant="secondary" onClick={handleOpen}>
         <BookOpenText className="size-6 lg:mr-2" />
