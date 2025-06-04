6fd70bfb23a9bd5bf0cd9a2ef23c9ea946eab8c8
Amit Schandillia <amit@schandillia.com>
2025-06-04 19:46:20 +0530

    style: improve bg color implementation on larger screens

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,11 +9,9 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <MobileHeader />
-      <Sidebar className="hidden lg:flex" />
-      <main className="h-full pt-[50px] lg:pt-0 lg:pl-[256px]">
-        <div className="mx-auto h-full max-w-[1056px] pt-6 bg-neutral-50 dark:bg-neutral-900">
-          {children}
-        </div>
+      <Sidebar className="hidden lg:flex bg-white dark:bg-black" />
+      <main className="h-full pt-[50px] lg:pt-0 lg:pl-[256px] bg-neutral-50 dark:bg-neutral-900">
+        <div className="mx-auto h-full max-w-[1056px] pt-6">{children}</div>
       </main>
     </>
   )
