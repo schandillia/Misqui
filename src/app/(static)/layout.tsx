377568f0c71c3bbeaf0f5dfc37f15ac4ac881888
Amit Schandillia <amit@schandillia.com>
2025-06-04 19:39:49 +0530

    refactor: make sidebar conditional on user auth status in static pages (terms, etc)

--- src/app/(static)/layout.tsx
+++ src/app/(static)/layout.tsx
@@ -1,19 +1,30 @@
 import { MobileHeader } from "@/components/mobile-header"
 import { Sidebar } from "@/components/sidebar"
+import { auth } from "@/auth"
 
 type Props = {
   children: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+  const session = await auth()
+  const showSidebar = !!session
+
   return (
     <>
       <MobileHeader />
-      <Sidebar className="hidden lg:flex" />
-      <main className="h-full lg:pl-[256px]">
+      {showSidebar && <Sidebar className="hidden lg:flex" />}
+      <main
+        className={
+          showSidebar
+            ? "h-full lg:pl-[256px]"
+            : "h-full bg-neutral-50 dark:bg-neutral-900"
+        }
+      >
         <div className="mx-auto h-full max-w-[1056px]">{children}</div>
       </main>
     </>
   )
 }
+
 export default Layout
