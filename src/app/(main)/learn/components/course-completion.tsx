d8e3a0160e10b9c74307e0f674bf923ea6110471
Amit Schandillia <amit@schandillia.com>
2025-06-23 13:59:29 +0530

    style: replace trophy icon with a proper svg and make course-completion prettier

--- src/app/(main)/learn/components/course-completion.tsx
+++ src/app/(main)/learn/components/course-completion.tsx
@@ -4,44 +4,39 @@ import { useEffect, useState } from "react"
 import Confetti from "react-confetti"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Trophy } from "lucide-react"
 import { HiOutlineRocketLaunch } from "react-icons/hi2"
+import Image from "next/image"
 
 type CourseCompletionProps = {
   courseTitle: string
 }
 
 const CourseCompletion = ({ courseTitle }: CourseCompletionProps) => {
-  const [showConfetti, setShowConfetti] = useState(true)
-
-  // Stop confetti after 5 seconds
-  useEffect(() => {
-    const timer = setTimeout(() => setShowConfetti(false), 5000)
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
     <div
       className="relative flex items-center justify-center h-[calc(100vh-74px)]
-        lg:h-[calc(100vh-24px)] bg-neutral-50 dark:bg-neutral-900 overflow-hidden"
+        lg:h-[calc(100vh-24px)] overflow-hidden"
     >
-      {showConfetti && (
-        <Confetti
-          width={typeof window !== "undefined" ? window.innerWidth : 1200}
-          height={typeof window !== "undefined" ? window.innerHeight : 800}
-          recycle={false}
-          numberOfPieces={500}
-          tweenDuration={10000}
-        />
-      )}
-      <Card className="w-full max-w-md mx-4 animate-in fade-in-50 z-10 dark:bg-black">
+      <Confetti
+        width={typeof window !== "undefined" ? window.innerWidth : 1200}
+        height={typeof window !== "undefined" ? window.innerHeight : 800}
+        recycle={false}
+        numberOfPieces={500}
+        tweenDuration={10000}
+        className="absolute inset-0 z-0"
+      />
+      <Card
+        className="w-full max-w-lg border-1 px-4 py-10 shadow-lg shadow-neutral-300 dark:border-2
+          dark:shadow-neutral-800 dark:bg-black z-10"
+      >
         <CardHeader className="text-center">
-          <div
-            className="mx-auto size-16 bg-yellow-50 dark:bg-yellow-950 rounded-full flex items-center
-              justify-center mb-4"
-          >
-            <Trophy className="size-10 text-yellow-500" />
-          </div>
+          <Image
+            src="/images/icons/trophy.svg"
+            alt="trophy"
+            className="size-16 text-yellow-500 mx-auto mb-4"
+            height={32}
+            width={32}
+          />
           <CardTitle className="text-3xl font-bold dark:text-neutral-300">
             Congratulations!
           </CardTitle>
